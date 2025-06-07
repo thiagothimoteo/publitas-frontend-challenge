894a547a70033631dbacd0930f79d781fbe9a03c
Thiago Thimoteo <thiago.thimoteo@gmail.com>
2025-06-07 13:27:17 -0300

    add styles

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const IMAGES_SOURCES = [
+  'https://placehold.jp/ff0000/ffffff/640x400.png?text=Image%201',
+  'https://placehold.jp/00ff00/ffffff/640x400.png?text=Image%202',
+  'https://placehold.jp/0000ff/ffffff/640x300.png?text=Image%203',
+];
